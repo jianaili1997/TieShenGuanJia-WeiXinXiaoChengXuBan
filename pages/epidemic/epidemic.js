a83a97e31c0b8aa20ab9ad396797e2e631bc615a
jianaili1997 <jianaili2014@163.com>
2021-01-20 21:25:39 +0800

    当日写当日提，疫情小功能--未完待续

--- pages/epidemic/epidemic.js
+++ pages/epidemic/epidemic.js
@@ -1,18 +1,33 @@
 // pages/epidemic/epidemic.js
-Page({
 
+const citys = {
+  浙江: ['杭州', '宁波', '温州', '嘉兴', '湖州'],
+  福建: ['福州', '厦门', '莆田', '三明', '泉州'],
+};
+Page({
   /**
    * 页面的初始数据
    */
   data: {
-
+    getdata: {}, // 获取的接口中的数据
+    columns: [{
+        values: Object.keys(citys),
+        className: 'column1',
+      },
+      {
+        values: citys['浙江'],
+        className: 'column2',
+        defaultIndex: 2,
+      },
+    ],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 获取可以查询的城市列表
+    this.getCity()
   },
 
   /**
@@ -62,5 +77,69 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+
+
+  /**
+   * 获取支持查询的城市
+   */
+  getCity() {
+    var that = this
+    wx.showLoading({
+      title: '加载中',
+    });
+    // 请求数据
+    wx.request({
+      // 聚合api
+      url: 'http://apis.juhe.cn/springTravel/citys?key=39ef9158b4dcf0ebb83e4cf98cbbcb31',
+      data: {},
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res)
+        wx.hideLoading();
+        if (res.statusCode == 200) {
+          if (res.data.result === null) {
+            wx.showModal({
+              title: '提示',
+              // content: "暂无数据",
+              content: res.data.reason,
+              success: function (res) {
+                that.setData({
+                  show: false
+                })
+              }
+            })
+          } else {
+            that.setData({
+              show: true,
+              getdata: res.data.result[0] // 取数组里面的第一组数；第二组数为相反的汇率转换
+            })
+          }
+
+        } else {
+          wx.showModal({
+            title: '提示',
+            content: res.data.reason,
+            success: function (res) {
+              that.setData({
+                show: false
+              })
+            }
+          })
+        }
+      }
+
+    })
+  },
+
+  onChange(event) {
+    const {
+      picker,
+      value,
+      index
+    } = event.detail;
+    picker.setColumnValues(1, citys[value[0]]);
+  },
 })
