3b68e30f7a250b6e0b20fd34cdc1205a98a9c088
jianaili1997 <jianaili2014@163.com>
2021-01-25 21:17:53 +0800

    将公共菜单项抽取出来

---
+++ common/menu-items.js
@@ -0,0 +1,69 @@
+const server = [{
+    img: "../../images/sfz.png",
+    openpath: "../../pages/IDCard/IDCard",
+    text: "身份证查询"
+  },
+  {
+    img: "../../images/yhk.png",
+    openpath: "../../pages/bankQuery/bankQuery",
+    text: "银行查询"
+  },
+  {
+    img: "../../images/gsd.png",
+    openpath: "../../pages/MobileHome/MobileHome",
+    text: "手机归属地"
+  },
+  {
+    img: "../../images/ip.png",
+    openpath: "../../pages/IP/IP",
+    text: "IP查询"
+  },
+  {
+    img: "../../images/sc.png",
+    openpath: "../../pages/figure/figure",
+    text: "身材计算"
+  },
+  {
+    img: "../../images/wsyt.png",
+    openpath: "../../pages/gojuuonn/gojuuonn",
+    text: "五十音图"
+  },
+  {
+    img: "../../images/cha.png",
+    openpath: "../../pages/pinyin/index",
+    text: "查拼音"
+  },
+  {
+    img: "../../images/clac.png",
+    openpath: "../../pages/calculator-index/index",
+    text: "计算器"
+  },
+  {
+    img: "../../images/hl.png",
+    openpath: "../../pages/cand/index",
+    text: "程序员黄历"
+  },
+  {
+    img: "../../images/gz.png",
+    openpath: "../../pages/game/game",
+    text: "摇骰子"
+  },
+  {
+    img: "../../images/2048.png",
+    openpath: "../../pages/2048/2048",
+    text: "2048"
+  },
+  {
+    img: "../../images/rate.png",
+    openpath: "../../pages/rate/rate",
+    text: "汇率"
+  },
+  {
+    img: "../../images/epidemic.png",
+    openpath: "../../pages/epidemic/epidemic",
+    text: "疫情"
+  }
+]
+module.exports = {
+  server
+}
