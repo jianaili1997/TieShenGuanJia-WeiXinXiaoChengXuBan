790359c30c165a02b594911faea073fd21cc07ce
jianaili1997 <jianaili2014@163.com>
2021-01-25 21:25:30 +0800

    首页菜单项的引入

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,5 @@
 // pages/index/index.js
+const common = require('../../common/menu-items')
 Page({
 
   /**
@@ -18,68 +19,7 @@ Page({
       img: "../../images/EmptyBanner3.jpg",
       openpath: "../../pages/MobileHome/MobileHome"
     }],
-    server: [{
-        img: "../../images/sfz.png",
-        openpath: "../../pages/IDCard/IDCard",
-        text: "身份证查询"
-      },
-      {
-        img: "../../images/yhk.png",
-        openpath: "../../pages/bankQuery/bankQuery",
-        text: "银行查询"
-      },
-      {
-        img: "../../images/gsd.png",
-        openpath: "../../pages/MobileHome/MobileHome",
-        text: "手机归属地"
-      },
-      {
-        img: "../../images/ip.png",
-        openpath: "../../pages/IP/IP",
-        text: "IP查询"
-      },
-      {
-        img: "../../images/sc.png",
-        openpath: "../../pages/figure/figure",
-        text: "身材计算"
-      },
-      {
-        img: "../../images/wsyt.png",
-        openpath: "../../pages/gojuuonn/gojuuonn",
-        text: "五十音图"
-      },
-      {
-        img: "../../images/cha.png",
-        openpath: "../../pages/pinyin/index",
-        text: "查拼音"
-      },
-      {
-        img: "../../images/clac.png",
-        openpath: "../../pages/calculator-index/index",
-        text: "计算器"
-      },
-      {
-        img: "../../images/hl.png",
-        openpath: "../../pages/cand/index",
-        text: "程序员黄历"
-      },
-      {
-        img: "../../images/gz.png",
-        openpath: "../../pages/game/game",
-        text: "摇骰子"
-      },
-      {
-        img: "../../images/2048.png",
-        openpath: "../../pages/2048/2048",
-        text: "2048"
-      },
-      {
-        img: "../../images/rate.png",
-        openpath: "../../pages/rate/rate",
-        text: "汇率"
-      }
-
-    ],
+    server: [],
     unserver: [{
         img: "../../images/kd.png",
         openpath: "../../pages/",
@@ -111,7 +51,9 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      server: common.server.slice(0, 12) // 截取数组的前十二个即 0-11
+    })
   },
 
   /**
