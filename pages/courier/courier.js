8a88b7d9edf1632fc8d3969d1c4046a1bf7f24a7
jianaili1997 <jianaili2014@163.com>
2021-01-28 21:48:47 +0800

    添加快递查询相关代码

--- pages/courier/courier.js
+++ pages/courier/courier.js
@@ -5,7 +5,55 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    company: [{
+        text: '申通',
+        value: 'shentong'
+      },
+      {
+        text: 'EMS',
+        value: 'ems'
+      },
+      {
+        text: '顺丰',
+        value: 'shunfeng'
+      },
+      {
+        text: ' 圆通',
+        value: 'yuantong'
+      },
+      {
+        text: '中通',
+        value: 'zhongtong'
+      },
+      {
+        text: ' 韵达',
+        value: 'yunda'
+      },
+      {
+        text: ' 天天',
+        value: 'tiantian'
+      },
+      {
+        text: ' 汇通',
+        value: 'huitongkuaidi'
+      },
+      {
+        text: ' 全峰',
+        value: 'quanfengkuaidi'
+      },
+      {
+        text: ' 德邦',
+        value: 'debangwuliu'
+      },
+      {
+        text: ' 宅急送',
+        value: 'zhaijisong'
+      }
+    ],
+    show: false,
+    value: 'shentong', // 默认显示申通
+    orderNo: '', // 快递单号
+    getdata: {} // 获取到的数据
   },
 
   /**
@@ -60,7 +108,47 @@ Page({
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function () {},
+  // 获取快递信息
+  getMsg() {
+    const that = this
+    if (this.data.orderNo === '') {
+      wx.showModal({
+        title: '提示',
+        content: '请输入快递单号'
+      })
+      return
+    }
+    wx.showLoading();
+    // 请求数据
+    wx.request({
+      url: `http://www.kuaidi100.com/query?type=${this.data.value}&postid=${this.data.orderNo}`,
+      data: {},
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res)
+        wx.hideLoading();
+        if (res.data.status === '200') {
+          that.setData({
+            show: true,
+            getdata: res.data
+          })
 
+        } else {
+          wx.showModal({
+            title: '提示',
+            content: res.message,
+            success: function (res) {
+              that.setData({
+                show: false
+              })
+            }
+          })
+        }
+      }
+    })
   }
+
 })
