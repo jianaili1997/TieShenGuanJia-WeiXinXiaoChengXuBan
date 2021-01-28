fb54a082a99158aa9fe487862e1355cb2569571f
jianaili1997 <jianaili2014@163.com>
2021-01-28 20:15:20 +0800

    轻松一刻页面初完善

--- pages/relaxMore/relaxMore.js
+++ pages/relaxMore/relaxMore.js
@@ -1,18 +1,21 @@
 // pages/relaxMore/relaxMore.js
+const common = require('../../common/menu-items')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    relax: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      relax: common.relax
+    })
   },
 
   /**
